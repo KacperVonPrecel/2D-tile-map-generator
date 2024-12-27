5c88a138a9f4b4cdbe5bd6bedb8fec7384428182
Kacper Skrodzki <kacskrogry@gmail.com>
2024-12-27 18:59:25 +0100

    Added .gitignore file and laid foundations for Terrain class

---
+++ terrain.py
@@ -0,0 +1,7 @@
+class Terrain:
+    def __init__(self, name, height, moisture, temperature, colour):
+        self._name = name
+        self._height = height
+        self._moisture = moisture
+        self._temperature = temperature
+        self._colour = colour
