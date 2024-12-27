39815b93e269af14f4efc50b7095a88033e83a87
Kacper Skrodzki <kacskrogry@gmail.com>
2024-12-27 21:31:20 +0100

    Added generation of noise maps(arrays) for three categories

--- twoD_tile_map.py
+++ twoD_tile_map.py
@@ -7,11 +7,31 @@ class TileMap:
     def __init__(self, height, width, seed):
         self._seed = seed
         self._map_array = np.zeros([height, width, 3], dtype=np.uint8)
+        self._height = height
+        self._width = width
 
     def generate_map(self):
-        height_noise_map = PerlinNoise(10, self._seed)
-        moisture_noise_map = PerlinNoise(4, self._seed)
-        temperature_noise_map = PerlinNoise(3, self._seed)
+        height_noise = PerlinNoise(10, self._seed)
+        moisture_noise = PerlinNoise(4, self._seed)
+        temperature_noise = PerlinNoise(3, self._seed)
+
+        hg = self._height
+        wd = self._width
+
+        height_array = [
+            [height_noise(row / hg, column / wd) for column in range(wd)]
+            for row in range(hg)
+            ]
+
+        moisture_array = [
+            [moisture_noise(row / hg, column / wd) for column in range(wd)]
+            for row in range(hg)
+            ]
+
+        temperature_array = [
+            [temperature_noise(row / hg, column / wd) for column in range(wd)]
+            for row in range(hg)
+            ]
 
     def load_map(self, path_from):
         pass
